280b478616c0bc6c50f30734f1661edb6dfddc16
NikaKochiashvili <nika.kochiashvili@gipa.ge>
2023-12-26 17:30:32 +0400

    auto-play feature added

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
   loop: true,
+  autoplay: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
